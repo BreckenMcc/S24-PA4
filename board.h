29aa36c34bc0a11d07006fbc489636db3d746bfb
Brecken <breckenmccrory@gmail.com>
2024-05-16 00:30:34 +0000

    board

---
+++ board.h
@@ -0,0 +1,26 @@
+#ifndef BOARD_H
+#define BOARD_H
+
+#include <iostream> 
+using namespace std;
+
+class Board{
+    private:
+        Ship* ships;
+        char* grid;
+    public:
+        Board();
+        Board(Ship*, char*);
+        Board(const Board&);
+
+        void setShips(Ship*);
+        void setGrid(char*);
+
+        Ship* getShips();
+        char* getGrid();
+
+        bool checkValidPlacement(Coordinate, Coordinate);
+        void placeShip(Ship, Coordinate, Coordinate);
+        void reciveAttack(Coordinate);
+        void displayBoard();
+};
