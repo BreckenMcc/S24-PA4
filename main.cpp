cf85fa42689fb3a5577b11d3421f72f775010f8f
Brecken <breckenmccrory@gmail.com>
2024-05-16 06:25:35 +0000

    main

--- main.cpp
+++ main.cpp
@@ -14,8 +14,46 @@ int main(){
     cout << "Welcome to Battleship!" << endl << "Here is your board!" << endl;
 
     //place Carrier Ship
-    int x, y;
-    cin >> x;
-    cin >> y;
-    Coordinate start(x,y);
+    bool validPlacement = false;
+    do(){
+        playerSelf.displayPlayerBoard();
+        cout << endl << "Please pick the starting coodinate for your Carrier (5 spaces)" << endl;
+        cout << "Input coordinates (x,y) for your choice: " << endl;
+
+        cin >> x;
+        cin >> y;
+        Coordinate start(x,y);
+
+        cout << endl << "Please pick the ending coodinate for your Carrier (5 spaces)" << endl;
+        cout << "Input coordinates (x,y) for your choice: " << endl;
+        cin >> x;
+        cin >> y;
+        Coordinate end(x,y);
+
+        if(start.getYValue == end.getYValue){
+            if(start.getXValue < end.getXValue){
+                validPlacement = playerSelf.checkValidPlacement(start, end);
+            }else{
+                validPlacement = playerSelf.checkValidPlacement(end, start);
+            }
+        }
+        if(start.getXValue == end.getXValue){
+            if(start.getYValue < end.getYValue){
+                validPlacement = playerSelf.checkValidPlacement(start, end);
+            }else{
+                validPlacement = playerSelf.checkValidPlacement(end, start);
+            }
+        }
+
+        if((start.getYValue - end.getYValue) != 5  (end.getYValue - start.getYValue) != 5  (end.getXValue - start.getXValue) != 5 || (start.getXValue - end.getXValue) != 5){
+            validPlacement = false; 
+        }
+
+        if(!validPlacement){
+            cout << "Please place your ships correctly" << endl;
+        }
+    }while(validPlacement == false);
+
+    ship carrier(5, false, start, end);
+    playerSelf.placeShip(carrier);
 }
